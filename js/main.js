31f96d73f20e9a6274e2f32f8aabb7adebeeb2cc
Mikael Gramont <mgramont@gmail.com>
2016-04-16 17:51:44 -0700

    Added TODO

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ let Transitioner = require('./transitioner');
 let WorldConfig = require('./world-config');
 
 const SIZE = 30;
-const RUNS = 1
 
 let logger = new Logger(window);
 let initialState = WorldConfig.BLOCK_TYPES.GRASS_CONTINUOUS;
@@ -13,8 +12,12 @@ let transitioner = new Transitioner(
 	initialState
 );
 let generator = new Generator(SIZE, transitioner);
-for(let i = 0; i < RUNS; i++) {
-	let blockTypes = generator.getAll();
-	logger.log("block types", i, blockTypes);
-	generator.reset(initialState);
-}
+let blockTypes = generator.getAll();
+logger.log("block types", blockTypes);
+
+// TODO: break up a canvas element into the right amount of blocks
+// pull up a sprite sheet, and render accordingly.
+
+// TODO: remove the different types of grass from the block type list
+// and start making distinctions later in a second pass, so we can
+// vary the types of things we render.
